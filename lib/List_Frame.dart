d72b5074c407eb921331cb8e801d1bbd1764983f
GuiQRSM <endbeob15@gmail.com>
2021-12-15 12:05:21 -0300

    Implement in the 'lib' folder the 'List_Frame' dart file with the 'package:flutter/material.dart' import already implemented and the 'stful' structure already called.

---
+++ lib/List_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListFrame extends StatefulWidget {
+  const ListFrame({Key? key}) : super(key: key);
+
+  @override
+  _ListFrameState createState() => _ListFrameState();
+}
+
+class _ListFrameState extends State<ListFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
