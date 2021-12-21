80357a2543ef0fb8202e7c42743aa29a909dfcd6
GuiQRSM <endbeob15@gmail.com>
2021-12-21 14:50:58 -0300

    Implements in the 'lib' folder a new dart file called 'DataWebList_Frame' with the import of 'material.dart' complete and the structure 'stful' returning the class 'Scaffold()' already called

---
+++ lib/DataWebList_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataWebList extends StatefulWidget {
+  const DataWebList({Key? key}) : super(key: key);
+
+  @override
+  _DataWebListState createState() => _DataWebListState();
+}
+
+class _DataWebListState extends State<DataWebList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
