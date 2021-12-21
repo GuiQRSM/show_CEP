4e23ea5a68cfc0798655021e9014b4957c810867
GuiQRSM <endbeob15@gmail.com>
2021-12-21 15:05:18 -0300

    It implements in the 'stful' structure the void method called '_getDataList() { }' with the asynchronous communication 'async' enabled.

--- lib/DataWebList_Frame.dart
+++ lib/DataWebList_Frame.dart
@@ -11,11 +11,15 @@ var pickColor = Color.fromRGBO(255, 214, 51, 1);
 var helperColor = Color.fromRGBO(204, 163, 0, 1);
 
 class _DataWebListState extends State<DataWebList> {
+
+  void _getDataList () async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: helperColor,
-
     );
   }
 }
